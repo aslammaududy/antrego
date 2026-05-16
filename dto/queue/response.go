c92fa9165be248d44211fc34832a0a40043447d6
Aslam <forjob.aslam@gmail.com>
2026-05-16 11:53:08 +0700

    forgot to provide estimated time on the response.

--- dto/queue/response.go
+++ dto/queue/response.go
@@ -1,22 +1,26 @@
 package queue
 
-import "antrego/models"
+import (
+	"antrego/models"
+)
 
 type Response struct {
-	ID          uint   `json:"id"`
-	BookingCode string `json:"booking_code"`
-	ClinicCode  string `json:"clinic_code"`
-	Number      string `json:"number"`
-	Status      string `json:"status"`
+	ID            uint   `json:"id"`
+	BookingCode   string `json:"booking_code"`
+	ClinicCode    string `json:"clinic_code"`
+	Number        string `json:"number"`
+	EstimatedTime string `json:"estimated_time"`
+	Status        string `json:"status"`
 }
 
 func NewResponse(queue models.Queue) Response {
 	return Response{
-		ID:          queue.ID,
-		BookingCode: queue.BookingCode,
-		ClinicCode:  queue.ClinicCode,
-		Number:      queue.Number,
-		Status:      queue.Status,
+		ID:            queue.ID,
+		BookingCode:   queue.BookingCode,
+		ClinicCode:    queue.ClinicCode,
+		Number:        queue.Number,
+		EstimatedTime: queue.EstimatedTime.Format("2006-01-02 15:04"),
+		Status:        queue.Status,
 	}
 }
 
